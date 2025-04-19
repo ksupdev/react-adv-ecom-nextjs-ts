ced93305fcf5ed45e48db806e54ff3c19e62d5b1
ksupdev <karoons.updev@gmail.com>
2025-04-19 18:45:36 +0700

    Set layout contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,18 +4,19 @@ import Link from "next/link";
 
 export default function ContactPage() {
     return (
-        <main>
-            <h2 className="font-k2d text-3xl text-blue-300">Contact us</h2>
+        <div className="mx-auto max-w-2xl pt-16 pb-10">
+            <h2 className="text-3xl text-blue-700">Contact us</h2>
             <p>สวัสดี contact Page</p>
             <p className="font-kanit">สวัสดี kanit contact Page</p>
             <p className={sarabun.className}>สวัสดี sarabun contact Page</p>
             <p className={kanit.className}>สวัสดี kanit contact Page</p>
+            <hr />
             <div className="m-10">
-                <Button variant="outline" >
+                <Button variant="outline" asChild >
                     <Link href="/">Go to Home Page</Link>
                 </Button>
             </div>
 
-        </main>
+        </div>
     )
 }
